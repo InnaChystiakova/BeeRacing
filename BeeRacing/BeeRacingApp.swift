e0fe9fb81aaa7e1897e698900028b30a2fd41d54
InnaChystiakova <inna_islyamova@ukr.net>
2024-11-08 15:36:58 +0000

    Initial commit

---
+++ BeeRacing/BeeRacingApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeeRacingApp.swift
+//  BeeRacing
+//
+//  Created by Inna Chystiakova on 08/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct BeeRacingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
